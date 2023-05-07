ad6b18e2ffc0372ea6ddaf7a92d9444af6704948
3azizo <mohamedreda7494591@gmail.com>
2023-05-07 18:22:18 +0300

    add leaflet library

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,21 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup('HI!').openPopup();
+    },
+    () => alert('could not get your position')
+  );
